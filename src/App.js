3ea01fa4de51f0539a8d3fb287754c56e30589ce
Muhammad Sheikh alard <m.000.m.mm44@gmail.com>
2024-08-25 22:21:05 +0300

    colorPaletteChanging

--- src/App.js
+++ src/App.js
@@ -1,58 +1,76 @@
-import Header from './Components/Header';
-import SideNav from './Components/SideNav';
-import Main from './Components/Main';
-import Section from './Components/Section';
-import ServCard from './Components/ServCard';
-import ProjCard from './Components/ProjCard';
-import Skill from './Components/Skill';
-import ContCard from './Components/ContCard';
-import Footer from './Components/Footer';
+import Header from "./Components/Header";
+import SideNav from "./Components/SideNav";
+import Main from "./Components/Main";
+import Section from "./Components/Section";
+import ServCard from "./Components/ServCard";
+import ProjCard from "./Components/ProjCard";
+import Skill from "./Components/Skill";
+import ContCard from "./Components/ContCard";
+import Footer from "./Components/Footer";
 
-import './Style/style.css';
-import './Style/variables.css';
-import './Style/dark_mode.css';
-import './Style/responsive.css';
-import './Assets/all.min.css';
+import "./Style/style.css";
+import "./Style/variables.css";
+import "./Style/dark_mode.css";
+import "./Style/responsive.css";
+import "./Assets/all.min.css";
 
-import { ServCards , ContCards , ProjCards } from './Data';
+import { ServCards, ContCards, ProjCards } from "./Data";
 
 function App() {
-  const ServCardsDataShow = ServCards.map((item , index) => 
-  <ServCard key={index}
-  iconClass={item.iconClass} 
-  title={item.title}
-  Descrption={item.Descrption}/>);
+  const ServCardsDataShow = ServCards.map((item, index) => (
+    <ServCard
+      key={index}
+      iconClass={item.iconClass}
+      title={item.title}
+      Descrption={item.Descrption}
+    />
+  ));
 
-  const ContCardsDataShow = ContCards.map((item , index) => 
-  <ContCard key={index}
-  iconClass={item.iconClass}
-  title={item.title}
-  Descrption={item.Descrption}
-  />);
+  const ContCardsDataShow = ContCards.map((item, index) => (
+    <ContCard
+      key={index}
+      iconClass={item.iconClass}
+      title={item.title}
+      Descrption={item.Descrption}
+    />
+  ));
 
-  const ProjCardsDataShow = []
+  const ProjCardsDataShow = [];
   for (let i = 0; i < ProjCards.length; i++) {
+    let ShowSkills = ProjCards[i].skills.map((item, index) => (
+      <Skill key={index} skill={item} />
+    ));
 
-      let ShowSkills = ProjCards[i].skills.map((item,index) =>
-      <Skill key={index} skill={item}/>)
-
-      ProjCardsDataShow[i]=<ProjCard key={i}
-      img={ProjCards[i].img} 
-      title={ProjCards[i].title}
-      category={ProjCards[i].category}
-      moreDetails={ProjCards[i].moreDetails}
-      skills={ShowSkills}/>
+    ProjCardsDataShow[i] = (
+      <ProjCard
+        key={i}
+        img={ProjCards[i].img}
+        title={ProjCards[i].title}
+        category={ProjCards[i].category}
+        moreDetails={ProjCards[i].moreDetails}
+        skills={ShowSkills}
+      />
+    );
   }
 
   return (
     <div className="App">
-      <Header/>
-        <SideNav/>
-        <Main/>
-        <Section title="Services" id="services" containt={ServCardsDataShow}/>
-        <Section title="Projects" id="projects" bgColor="var(--color-yello)" containt={ProjCardsDataShow}/>
-        <Section title="Let's work together" id="contact" containt={ContCardsDataShow}/>
-      <Footer/>
+      <Header />
+      <SideNav />
+      <Main />
+      <Section title="Services" id="services" containt={ServCardsDataShow} />
+      <Section
+        title="Projects"
+        id="projects"
+        bgColor="var(--color-primary)"
+        containt={ProjCardsDataShow}
+      />
+      <Section
+        title="Let's work together"
+        id="contact"
+        containt={ContCardsDataShow}
+      />
+      <Footer />
     </div>
   );
 }
--- src/Components/ServCard.js
+++ src/Components/ServCard.js
@@ -1,13 +1,32 @@
-export default  function ServCard(props){
-
-    return <div className="card" style={{width:'40em',display:'flex',alignItems:'center',border:'0.2em solid #FDB724'}}>
-    <div style={{}}>
-        <div className="icon"><i className={props.iconClass}></i></div>
-        <div className="info">
-            <h3>{props.title}</h3>
+export default function ServCard(props) {
+  return (
+    <div
+      className="card"
+      style={{
+        width: "40em",
+        display: "flex",
+        alignItems: "center",
+        border: "0.2em solid #3c76b9",
+      }}
+    >
+      <div style={{}}>
+        <div className="icon">
+          <i className={props.iconClass}></i>
         </div>
+        <div className="info">
+          <h3>{props.title}</h3>
+        </div>
+      </div>
+      <div
+        style={{
+          width: "1.1em",
+          height: "15em",
+          borderRadius: "100em",
+          backgroundColor: "#3c76b9",
+          margin: "0 2em",
+        }}
+      ></div>
+      <p style={{ fontSize: "1.2em" }}>{props.Descrption}</p>
     </div>
-    <div style={{width:'1.1em',height:'15em',borderRadius:'100em',backgroundColor:'#FDB724',margin:'0 2em'}}></div>    
-        <p style={{fontSize:'1.2em'}}>{props.Descrption}</p>
-</div>;
+  );
 }
